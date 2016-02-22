8a3854f38f3635c2465bc900f702c7d72e0b3513
jstofferahn <stofferahnjustin@gmail.com>
2016-02-22 13:42:27 -0700

    Start of googleAuth

---
+++ ionic/www/js/controllers/loginCtrl.js
@@ -0,0 +1,148 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.controller('WelcomeCtrl', function($scope, $state, googleAuthService, $ionicLoading) {
+  // This method is executed when the user press the "Sign in with Google" button
+  $scope.googleSignIn = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
+    });
+
+    window.plugins.googleplus.login(
+      {},
+      function (user_data) {
+        // For the purpose of this example I will store user data on local storage
+        googleAuthService.setUser({
+          userID: user_data.userId,
+          name: user_data.displayName,
+          email: user_data.email,
+          picture: user_data.imageUrl,
+          accessToken: user_data.accessToken,
+          idToken: user_data.idToken
+        });
+
+        $ionicLoading.hide();
+        $state.go('app.home');
+      },
+      function (msg) {
+        $ionicLoading.hide();
+      }
+    );
+  };
+})

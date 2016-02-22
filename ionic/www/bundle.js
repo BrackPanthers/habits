ab77acf86dd75771e92e96cefc20de5abff53fdf
Jeff Garcia <jeffrgarcia24@gmail.com>
2016-02-22 14:47:55 -0700

    commiting

--- ionic/www/bundle.js
+++ ionic/www/bundle.js
@@ -19,14 +19,31 @@ habitApp.run(function($ionicPlatform) {
   });
 })
 
+
+
+
+
+
+
 habitApp.constant('constants', function() {
   return {
     baseServerUrl: 'http://localhost:3000'
   }
 });
 
+angular.module('habitApp', [])
+.service('loginService', function() {
+  // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+  var setUser = function(user_data) {
+    window.localStorage.starter_facebook_user = JSON.stringify(user_data);
+  };
 
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_facebook_user || '{}');
+  };
 
-
-
-
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+});

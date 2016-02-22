e0fdc25a0c5f6a56be261435d297b2c937147fb8
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-02-22 15:12:12 -0700

    reverting commits

--- ionic/www/bundle.js
+++ ionic/www/bundle.js
@@ -100,3 +100,20 @@ habitApp.constant('constants', function() {
     baseServerUrl: 'http://localhost:3000'
   }
 });
+
+angular.module('habitApp', [])
+.service('loginService', function() {
+  // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+  var setUser = function(user_data) {
+    window.localStorage.starter_facebook_user = JSON.stringify(user_data);
+  };
+
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_facebook_user || '{}');
+  };
+
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+});

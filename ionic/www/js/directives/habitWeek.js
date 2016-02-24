8b07cd99d195e550277f4e0b90f01c84030cc414
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-02-23 21:07:28 -0700

    build out habits section of profile view to be more interactive and demo ux

--- ionic/www/js/directives/habitWeek.js
+++ ionic/www/js/directives/habitWeek.js
@@ -1,6 +1,6 @@
 habitApp.directive('habitWeek', function() {
   return {
-    templateUrl: './../../views/habit-week.html',
+    templateUrl: './views/habit-week.html',
     controller: 'habitWeekCtrl',
     scope: {
       habitData: '='

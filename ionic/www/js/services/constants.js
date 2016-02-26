c4dc18fac212d33e19e0221cff6d9c65882469d5
jstofferahn <stofferahnjustin@gmail.com>
2016-02-26 13:46:50 -0700

    added ned plugins

--- ionic/www/js/services/constants.js
+++ ionic/www/js/services/constants.js
@@ -1,3 +1,3 @@
 habitApp.constant('constants', {
-  baseServerUrl: 'http://192.168.1.185:3000'// change to remote when hosting
+  baseServerUrl: 'http://192.168.1.66:3005'// change to remote when hosting
 });

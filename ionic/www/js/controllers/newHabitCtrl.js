2738e5cfab15333c3b0c815837d94c75d9118913
Jeff Garcia <jeffrgarcia24@gmail.com>
2016-02-29 15:49:18 -0700

    changed some values

--- ionic/www/js/controllers/newHabitCtrl.js
+++ ionic/www/js/controllers/newHabitCtrl.js
@@ -2,15 +2,17 @@ habitApp.controller('newHabitCtrl', function ($scope, habitService, authSvc) {
     $scope.addNewHabit = function (habit) {
     	habit.user_id = authSvc.getCurrUser().user_id;
         habitService.postNewHabit(habit).then(function (res) {
-            $scope.newHabit = '';
+            $scope.newHabit = {
+				goal_point: {} 
+			};
         });
-        console.log(habit)
+        // console.log(habit)
     }
 
 	$scope.newHabit = {
 		goal_point: {} 
 	};
-	
+
     $scope.newHabit.goal_point.frequency = 0;
 
     $scope.categories = [

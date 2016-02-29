90edf6e134a5789e9205d089734b868e4bb81134
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-02-29 09:35:56 -0700

    constants update

--- ionic/www/js/services/constants.js
+++ ionic/www/js/services/constants.js
@@ -1,4 +1,7 @@
 habitApp.constant('constants', {
+  // IP/port of server running/hosted on digital ocean
   baseServerUrl: 'http://198.199.116.86:3000',
+  // Your local IP/port, for testing w/ nodemon
+  // baseServerUrl: 'http://198.199.116.86:3000',
   fbAppId: '461176754071244'
 });

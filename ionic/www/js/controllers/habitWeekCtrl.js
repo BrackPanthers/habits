68f383399275282fe6b9bcf3ce1df3f06af43209
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-03-02 14:58:13 -0700

    consolidated habit detail state into a modal that is part of profile state. This enables us to better access habit data in habit detail view. Added css animation to modal that retains effect of sliding from right for habit details.

--- ionic/www/js/controllers/habitWeekCtrl.js
+++ ionic/www/js/controllers/habitWeekCtrl.js
@@ -1,8 +1,168 @@
-habitApp.controller('habitWeekCtrl', function($scope) {
-  $scope.test = 'HABIT WEEK CTRL CONNECT';
+habitApp.controller('habitWeekCtrl', function($scope, $ionicModal) {
 
   $scope.dayBoxArr = [false, false, false, false, false, false, false];
   $scope.toggleDay = function(dayIndex) {
     $scope.dayBoxArr[dayIndex] = !$scope.dayBoxArr[dayIndex];
   };
+
+  $ionicModal.fromTemplateUrl('./views/habit-detail-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-right'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  //// CONTROLLER DATA IMPORTED FROM HABIT-DETAIL CTRL ////
+  //// TURN CALENDAR INTO DIRECTIVE AND MOVE THIS TO NEW CTRL ///
+
+  //TEST DATA//
+    $scope.datesArr = [
+      {
+        num: 1,
+        completed: true
+      },
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1,
+      completed: true},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1,
+      completed: true},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1},
+      {num: 1}
+    ];
+    var startDate = moment();
+    var startDayOfWeek = startDate.format('d');
+    var startDateNum = startDate.format('D');
+    // console.log(startDateNum);
+
+    //SQUARE COLOR LOGIC//
+    for (var i = 0; i < $scope.datesArr.length; i++) {
+      if ( $scope.datesArr[i].completed === true) {
+        $scope.datesArr[i]['class'] = 'green-highlight';
+      }
+    }
+
+
+  //DEFINE START DATE//
+  var startIndex;
+  for (var i = 0; i < $scope.datesArr.length; i++) {
+    if (i == startDayOfWeek) {
+      $scope.datesArr[i].date = startDateNum;
+      startIndex = i;
+      // console.log(startDateNum);
+    }
+  }
+
+  //POPULATE CALENDAR DATES//
+  for (var i = startIndex + 1; i < $scope.datesArr.length; i++) {
+      $scope.datesArr[i]['date'] = moment().add(i - (startIndex), 'days').format('D');
+  }
+
+  var count = 1;
+  for (var i = startIndex - 1; i >= 0; i--) {
+    $scope.datesArr[i]['date'] = moment().subtract(count, 'days').format('D');
+    count++;
+  }
+
+
+    // $scope.days = [
+    //   {
+    //     day: 'Sunday',
+    //     letter: 'S',
+    //     class: ''
+    //   },
+    //   {
+    //     day: 'Monday',
+    //     letter: 'M',
+    //     class: ''
+    //   },
+    //   {
+    //     day: 'Tuesday',
+    //     letter: 'T',
+    //     class: ''
+    //   },
+    //   {
+    //     day: 'Wednesday',
+    //     letter: 'W',
+    //     class: ''
+    //   },
+    //   {
+    //     day: 'Thursday',
+    //     letter: 'T',
+    //     class: ''
+    //   },
+    //   {
+    //     day: 'Friday',
+    //     letter: 'F',
+    //     class: ''
+    //   },
+    //   {
+    //     day: 'Saturday',
+    //     letter: 'S',
+    //     class: ''
+    //   }
+    // ]
+    //
+    // var currentDay = moment().format('dddd');
+    // for (var i = 0; i < $scope.days.length; i++) {
+    //   if ($scope.days[i].day === currentDay) {
+    //     $scope.days[i].class = 'current-day';
+    //   }
+    // }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  $scope.show = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your habit',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 3000);
+
+  };
+
+  $scope.deleteHabit = function() {
+   habitService.deleteHabit($stateParams.habitId);
+  }
 });

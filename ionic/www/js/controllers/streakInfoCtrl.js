27854b4f1a1873ca4036f89019f7a7359378a97e
Jeff Garcia <jeffrgarcia24@gmail.com>
2016-03-03 14:43:23 -0700

    finished daily streak check

--- ionic/www/js/controllers/streakInfoCtrl.js
+++ ionic/www/js/controllers/streakInfoCtrl.js
@@ -6,13 +6,15 @@ habitApp.controller('streakInfoCtrl', function($scope, $ionicActionSheet, $timeo
   	if (timeFrame === 'day') {
   		console.log('run daily checkStreak')
   		checkStreak(logs);
+      currentDayStreak(logs);
   	}
   	else {
   		console.log('run weekly checkStreak')
-  		weekHighestStreak(logs);
+  		// weekHighestStreak(logs);
   	}
   }
 
+// checking daily current and highest streaks
   var checkStreak = function(logs) {
     console.log('Starting check for Habit:', $scope.habitData.text);
     var highestStreak = 1;
@@ -21,7 +23,7 @@ habitApp.controller('streakInfoCtrl', function($scope, $ionicActionSheet, $timeo
     for (var i = 0; i < logs.length - 1; i++) {
 		var date1 = moment(logs[i]).format("MM-DD-YYYY");
 		var date2 = moment(logs[i + 1]).subtract(1, 'days').format("MM-DD-YYYY");
-		// console.log("Comparing data", date1, date2)
+		console.log("Comparing data", date1, date2)
 		if ( date2 === date1 ) {
 			console.log("adding to streak")
 			streak++; 
@@ -39,30 +41,48 @@ habitApp.controller('streakInfoCtrl', function($scope, $ionicActionSheet, $timeo
     $scope.highestStreak = highestStreak;
   }
 
-  var weekHighestStreak = function(logs) {
-  	console.log('Starting check for Habit:', $scope.habitData.text);
-  	var weekStreak = 1;
-  	var highestWeek = 1;
-  	console.log('Length: ', logs.length);
-  	for(var i = 0; i < logs.length - 1; i++) {
-  		var week1 = moment(logs[i]).format("MM-DD-YYYY");
-  		var week2 = moment(logs[i + 1]).subtract(1, 'week').format('MM-DD-YYYY');
-  		console.log("Comparing data", week1, week2)
-  		if (week2 === week1) {
-  			console.log("adding to streak")
-  			weekStreak++;
-  				if (weekStreak > highestWeek) {
-  					highestWeek = weekStreak;
-  				}
-  		}
-  		else {
-  			console.log('streak ended');
-  			weekStreak = 1;
-  		}
-  	}
-  	console.log('fin', highestWeek);
-  	$scope.highestWeek = highestWeek;
+  var currentDayStreak = function(logs) {
+    var dayStreak = 1;
+    console.log('Length: ', logs.length);
+    for (var i = logs.length - 1; i > 0; i--) {
+      var lastDay = moment(logs[i]).format("MM-DD-YYYY");
+      var nextDay = moment(logs[i - 1]).add(1, 'days').format("MM-DD-YYYY");
+      console.log("Comparing data", lastDay, nextDay)
+      if ( nextDay === lastDay) {
+        dayStreak++;
+      }
+      else {
+        break;
+      }
+    }
+    $scope.dayStreak = dayStreak;
+    console.log("run currentDayStreak", dayStreak)
   }
+
+  // var weekHighestStreak = function(logs) {
+  // 	console.log('Starting check for Habit:', $scope.habitData.text);
+  // 	var weekStreak = 1;
+  // 	var highestWeek = 1;
+  // 	console.log('Length: ', logs.length);
+  // 	for(var i = 0; i < logs.length - 1; i++) {
+  // 		var week1 = moment(logs[i]).format("MM-DD-YYYY");
+  // 		var week2 = moment(logs[i + 1]).subtract(1, 'week').format('MM-DD-YYYY');
+  // 		console.log("Comparing data", week1, week2)
+  // 		if (week2 === week1) {
+  // 			console.log("adding to streak")
+  // 			weekStreak++;
+  // 				if (weekStreak > highestWeek) {
+  // 					highestWeek = weekStreak;
+  // 				}
+  // 		}
+  // 		else {
+  // 			console.log('streak ended');
+  // 			weekStreak = 1;
+  // 		}
+  // 	}
+  // 	console.log('fin', highestWeek);
+  // 	$scope.highestWeek = highestWeek;
+  // }
 
 
 

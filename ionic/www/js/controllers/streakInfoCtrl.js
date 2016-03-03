671050246e80b098ae304efab13983d836e56818
Jeff Garcia <jeffrgarcia24@gmail.com>
2016-03-03 14:56:32 -0700

    starting weekly streak

--- ionic/www/js/controllers/streakInfoCtrl.js
+++ ionic/www/js/controllers/streakInfoCtrl.js
@@ -4,50 +4,50 @@ habitApp.controller('streakInfoCtrl', function($scope, $ionicActionSheet, $timeo
 
   var compareGoalTimeFrame = function(timeFrame) {
   	if (timeFrame === 'day') {
-  		console.log('run daily checkStreak')
+  		// console.log('run daily checkStreak')
   		checkStreak(logs);
       currentDayStreak(logs);
   	}
   	else {
-  		console.log('run weekly checkStreak')
+  		// console.log('run weekly checkStreak')
   		// weekHighestStreak(logs);
   	}
   }
 
 // checking daily current and highest streaks
   var checkStreak = function(logs) {
-    console.log('Starting check for Habit:', $scope.habitData.text);
+    // console.log('Starting check for Habit:', $scope.habitData.text);
     var highestStreak = 1;
     var streak = 1;
-    console.log('Length: ', logs.length);
+    // console.log('Length: ', logs.length);
     for (var i = 0; i < logs.length - 1; i++) {
 		var date1 = moment(logs[i]).format("MM-DD-YYYY");
 		var date2 = moment(logs[i + 1]).subtract(1, 'days').format("MM-DD-YYYY");
-		console.log("Comparing data", date1, date2)
+		// console.log("Comparing data", date1, date2)
 		if ( date2 === date1 ) {
-			console.log("adding to streak")
+			// console.log("adding to streak")
 			streak++; 
         		if (streak > highestStreak) {
           			highestStreak = streak;
-          			console.log('set new highestStreak', highestStreak);
+          			// console.log('set new highestStreak', highestStreak);
         		}
 		}
       else {
-        console.log('streak ended');
+        // console.log('streak ended');
         streak = 1;
       }
     }
-    console.log('fin', highestStreak);
+    // console.log('fin', highestStreak);
     $scope.highestStreak = highestStreak;
   }
 
   var currentDayStreak = function(logs) {
     var dayStreak = 1;
-    console.log('Length: ', logs.length);
+    // console.log('Length: ', logs.length);
     for (var i = logs.length - 1; i > 0; i--) {
       var lastDay = moment(logs[i]).format("MM-DD-YYYY");
       var nextDay = moment(logs[i - 1]).add(1, 'days').format("MM-DD-YYYY");
-      console.log("Comparing data", lastDay, nextDay)
+      // console.log("Comparing data", lastDay, nextDay)
       if ( nextDay === lastDay) {
         dayStreak++;
       }
@@ -56,7 +56,7 @@ habitApp.controller('streakInfoCtrl', function($scope, $ionicActionSheet, $timeo
       }
     }
     $scope.dayStreak = dayStreak;
-    console.log("run currentDayStreak", dayStreak)
+    // console.log("run currentDayStreak", dayStreak)
   }
 
   // var weekHighestStreak = function(logs) {
@@ -83,6 +83,12 @@ habitApp.controller('streakInfoCtrl', function($scope, $ionicActionSheet, $timeo
   // 	console.log('fin', highestWeek);
   // 	$scope.highestWeek = highestWeek;
   // }
+
+  var weekHighestStreak = function(logs) {
+    var logsPerWeek = [];
+    var weekStreak = 1;
+    var highestWeek = 1;
+  }
 
 
 

569716491a147bb3ee4386375ed91dc7a3478800
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-03-10 10:57:11 -0700

    changed to server url

--- ionic/www/js/services/constants.js
+++ ionic/www/js/services/constants.js
@@ -1,8 +1,8 @@
 habitApp.constant('constants', {
 
   // IP/port of server running/hosted on digital ocean
-  // baseServerUrl: 'http://198.199.116.86:3000',
+  baseServerUrl: 'http://198.199.116.86:3000',
   // Your local IP/port, for testing w/ nodemon
-  baseServerUrl: 'http://192.168.1.185:3000',
+  // baseServerUrl: 'http://192.168.1.185:3000',
   fbAppId: '461176754071244'
 });
